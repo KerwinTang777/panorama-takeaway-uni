b30db2b68e8e14ffd114b4e86aefd172d5b5890d
KerwinTang777 <kerwintang777@gmail.com>
2024-04-07 13:44:21 +0800

    FirstCommit
    FirstCommit

---
+++ App.vue
@@ -0,0 +1,28 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*Login图片*/
+	.image-bg {
+	    position: absolute;
+	    z-index: -1;
+	    left: 0;
+	    right: 0;
+	    bottom: 0;
+	    right: 0;
+	    width: 100%;
+	    height: 140rpx;
+	}
+	/*每个页面公共css */
+</style>
---
+++ config.js
@@ -0,0 +1,12 @@
+const apiConfig = {
+	dev: {
+		//本地URL位置
+		baseUrl: 'http://localhost:9090'
+	},
+	prod: {
+		//生产环境URL位置
+		baseUrl: 'http://:9090'
+	}
+}
+
+export default apiConfig
